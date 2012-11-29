cc3ff40acf5f5dc7a28d4c380df2513c70ce693f
Bjørn Rohde Jensen <brj@statsbiblioteket.dk>
2012-11-29 11:56:34 +0000

    reworked ticket store to be stateful and split ticket deletion out from ticket retrieval

--- lib/Cas/TicketStore/FileSystemTicketStore.php
+++ lib/Cas/TicketStore/FileSystemTicketStore.php
@@ -2,40 +2,50 @@
 
 class sspmod_sbcasserver_Cas_TicketStore_FileSystemTicketStore {
 
+  private $pathToTicketDirectory;
+
   public function __construct($storeConfig) {
+    if(!is_string($config['directory'])) {
+      throw new Exception('Missing or invalid directory option in config.');
+    }
+
+    if (!is_dir($config['directory'])) 
+      throw new Exception('Directory for CAS Server ticket storage [' . $config['directory'] . '] does not exists. ');
+		
+    if (!is_writable($config['directory'])) 
+      throw new Exception('Directory for CAS Server ticket storage [' . $config['directory'] . '] is not writable. ');
+
+    $pathToTicketDirectory = preg_replace('/\/$/','',$config['directory']);
   }
 
-  public function storeTicket($ticket, $path, $value ) {
+  public function createTicket($ticket, $value ) {
 
-    if (!is_dir($path)) 
-      throw new Exception('Directory for CAS Server ticket storage [' . $path . '] does not exists. ');
-		
-    if (!is_writable($path)) 
-      throw new Exception('Directory for CAS Server ticket storage [' . $path . '] is not writable. ');
-
-    $filename = $path . '/' . $ticket;
+    $filename = $pathToTicketDirectory . '/' . $ticket;
     file_put_contents($filename, serialize($value));
   }
 
-  public function retrieveTicket($ticket, $path, $unlink = true) {
+  public function getTicket($ticket) {
 
     if (!preg_match('/^(ST|PT|PGT)-?[a-zA-Z0-9]+$/D', $ticket)) throw new Exception('Invalid characters in ticket');
 
-    if (!is_dir($path)) 
-      throw new Exception('Directory for CAS Server ticket storage [' . $path . '] does not exists. ');
-
-    $filename = $path . '/' . $ticket;
+    $filename = $pathToTicketDirectory . '/' . $ticket;
 
     if (!file_exists($filename))
       throw new Exception('Could not find ticket');
 	
     $content = file_get_contents($filename);
 	
-    if ($unlink) {
+    return unserialize($content);
+  }
+
+  public function deleteTicket($ticket) {
+    if (!preg_match('/^(ST|PT|PGT)-?[a-zA-Z0-9]+$/D', $ticket)) throw new Exception('Invalid characters in ticket');
+
+    $filename = $pathToTicketDirectory . '/' . $ticket;
+
+    if (file_exists($filename)) {
       unlink($filename);
     }
-	
-    return unserialize($content);
   }
   }
 ?>
