a6607e40ed7c57238e0fa702a40b2e05bba25296
Bjørn Rohde Jensen <brj@statsbiblioteket.dk>
2013-02-19 11:33:49 +0000

    removed the restriction on authsource names

--- www/login.php
+++ www/login.php
@@ -19,14 +19,14 @@ $config = SimpleSAML_Configuration::getInstance();
 $casconfig = SimpleSAML_Configuration::getConfig('module_sbcasserver.php');
 
 $legal_service_urls = $casconfig->getValue('legal_service_urls');
+
 if (!checkServiceURL($service, $legal_service_urls))
 	throw new Exception('Service parameter provided to CAS server is not listed as a legal service: [service] = ' . $service);
 
 $auth = $casconfig->getValue('authsource', 'saml2');
-if (!in_array($auth, array('saml2', 'shib13')))
- 	throw new Exception('CAS Service configured to use [auth] = ' . $auth . ' only [saml2,shib13] is legal.');
- 
+
 $as = new SimpleSAML_Auth_Simple($auth);
+
 if (!$as->isAuthenticated()) {
 	$params = array(
 		'ForceAuthn' => $forceAuthn,
