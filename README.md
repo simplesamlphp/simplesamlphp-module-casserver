ea4af920b776e3c971ee59f71d5992f6c79b885b
Bjorn R. Jensen <bjensen@fastmail.fm>
2013-07-02 09:56:56 +0200

    added lgpl license and snippet to all source headers

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ simpleSAMLphp-sbcasserver
 =========================
 
 CAS 1.0 and 2.0 compliant CAS server module for simpleSAMLphp
+
+This is the simpleSAMLphp CAS server module developed at the State and University Library in Aarhus Denmark.
+The module is a fork of an old version of the CAS module shipped with simpleSAMLphp which has undergone a couple of
+iterations of refactoring, bugfixes and enhancements. For details see the ChangeLog in the doc directory.
+
+All files are rewritten based on work by Dubravko Voncina. See Google Groups discussion in this thread:
+http://groups.google.com/group/simplesamlphp/browse_thread/thread/4c655d169532650a
+
+This work is licensed under a Creative Commons GNU Lesser General Public License License.
+
--- www/cas.php
+++ www/cas.php
@@ -1,5 +1,23 @@
 <?php
 /*
+*    simpleSAMLphp-sbcasserver is a CAS 1.0 and 2.0 compliant CAS server in the form of a simpleSAMLphp module
+*
+*    Copyright (C) 2013  Bjorn R. Jensen
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*
 * Frontend for login.php, validate.php, serviceValidate.php, logout.php, proxy and proxyValidate. It allows them to be
 * called as cas.php/login, cas.php/validate, cas.php/serviceValidate, cas.php/logout, cas.php/proxy and cas.php/proxyValidate
 * and is meant for clients like phpCAS which expects one configured prefix which it appends login, validate, serviceValidate and logout to.
