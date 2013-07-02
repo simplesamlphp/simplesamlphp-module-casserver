24e3d2e55752cacca2bf46748641a3724a347855
Bjorn R. Jensen <bjensen@fastmail.fm>
2013-07-02 10:03:52 +0200

    removed support for storing tickets in proprietary attribute store

--- config-templates/module_sbcasserver.php
+++ config-templates/module_sbcasserver.php
@@ -22,11 +22,6 @@ $config = array(
 
         //'class' => 'sbcasserver:MemCacheTicketStore',
         //'prefix' => 'bibsys',
-
-        //'class' => 'sbcasserver:AttributeStoreTicketStore',
-        //'attributeStoreUrl' => 'http://devel06:9561/attributestore/services/json/store/',
-        //'attributeStoreDeleteUrl' => 'http://devel06:9561/attributestore/services/store/',
-        //'attributeStorePrefix' => 'sbmobile.cas',
     ),
 
     'attrname' => 'eduPersonPrincipalName', // 'eduPersonPrincipalName',
