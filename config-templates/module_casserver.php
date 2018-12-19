2ea6c5d34b2a085ec8e837a8cf68c81531e4440f
Tim van Dijen <tvdijen@gmail.com>
2018-12-19 18:00:00 +0100

    Shorthand arrays

--- config-templates/module_casserver.php
+++ config-templates/module_casserver.php
@@ -4,29 +4,37 @@
  * 
  */
 
-$config = array(
+$config = [
     'authsource' => 'casserver',
 
     /* Scopes are named sets of entityIDs to be used for scoping. If a named scope is provided during login, then the
        IdPs listed by the disco service will be restricted to entityIDs in the specified set. */
-    'scopes' => array('mobile' => array('https://idp1.domain:1234/saml2/idp/metadata.php',
-                                        'https://idp2.domain:5678/saml2/idp/metadata.php'),
-                      'desktop' => array('https://idp3.domain:1234/saml2/idp/metadata.php', 
-                                         'https://idp4.domain:5678/saml2/idp/metadata.php')),
-
-    'legal_service_urls' => array( //Any service url string matching any of the following prefixes is accepted
+    'scopes' =>[
+        'mobile' => [
+            'https://idp1.domain:1234/saml2/idp/metadata.php',
+            'https://idp2.domain:5678/saml2/idp/metadata.php'
+        ],
+        'desktop' => [
+            'https://idp3.domain:1234/saml2/idp/metadata.php', 
+            'https://idp4.domain:5678/saml2/idp/metadata.php'
+        ]
+    ],
+    'legal_service_urls' => [
+         //Any service url string matching any of the following prefixes is accepted
         'http://host1.domain:1234/path1',
         'https://host2.domain:5678/path2/path3',
-    ),
+    ],
 
-    'legal_target_service_urls' => array( //Any target service url string matching any of the following prefixes is accepted
+    'legal_target_service_urls' => [
+         //Any target service url string matching any of the following prefixes is accepted
         'http://host3.domain:4321/path4',
         'https://host4.domain:8765/path5/path6',
         // So is regex
         '|^https://.*\.domain.com/|'
-    ),
+    ],
 
-    'ticketstore' => array( //defaults to filesystem ticket store using the directory 'ticketcache'
+    'ticketstore' => [
+         //defaults to filesystem ticket store using the directory 'ticketcache'
         'class' => 'casserver:FileSystemTicketStore', //Not intended for production
         'directory' => 'ticketcache',
 
@@ -44,18 +52,18 @@ $config = array(
 
         //'class' => 'casserver:RedisTicketStore',
         //'prefix' => 'some_prefix',
-    ),
+    ],
 
     'attrname' => 'eduPersonPrincipalName', // 'eduPersonPrincipalName',
     'attributes' => true, // enable transfer of attributes, defaults to false
-    'attributes_to_transfer' => array('eduPersonPrincipalName'), // set of attributes to transfer, defaults to all
+    'attributes_to_transfer' => ['eduPersonPrincipalName'], // set of attributes to transfer, defaults to all
 
-    'base64attributes' => true, //base64 encode transferred attributes, defaults to false
+    'base64attributes' => true, // base64 encode transferred attributes, defaults to false
     'base64_attributes_indicator_attribute' => 'base64Attributes', /*add an attribute with the value of the base64attributes
                                                                      configuration parameter to the set of transferred attributes.
                                                                      Defaults to not adding an indicator attribute. */
 
-    'enable_logout' => true, //enable CAS logout, defaults to false
+    'enable_logout' => true, // enable CAS logout, defaults to false
     'skip_logout_page' => true, /*perform a redirect instead of showing a logout page with a link to the location
                                   given in the url parameter, defaults to false. Skipping the logout page makes the
                                   url query parameter to CAS logout mandatory for obvious reasons.*/
@@ -63,4 +71,4 @@ $config = array(
     'service_ticket_expire_time' => 5, //how many seconds service tickets are valid for, defaults to 5
     'proxy_granting_ticket_expire_time' => 600, //how many seconds proxy granting tickets are valid for at most, defaults to 3600
     'proxy_ticket_expire_time' => 5, //how many seconds proxy tickets are valid for, defaults to 5
-);
+];
