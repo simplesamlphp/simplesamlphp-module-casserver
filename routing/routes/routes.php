44a3b0ed01a71e26b74953e2efbbdbad099cea2d
Ioannis Igoumenos <ioigoume@gmail.com>
2024-11-21 19:16:40 +0200

    LoggedInController.php

--- routing/routes/routes.php
+++ routing/routes/routes.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 use SimpleSAML\Module\casserver\Codebooks\LegacyRoutesEnum;
 use SimpleSAML\Module\casserver\Codebooks\RoutesEnum;
 use SimpleSAML\Module\casserver\Controller\Cas10Controller;
+use SimpleSAML\Module\casserver\Controller\LoggedInController;
 use SimpleSAML\Module\casserver\Controller\LoggedOutController;
 use SimpleSAML\Module\casserver\Controller\LogoutController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
@@ -24,6 +25,8 @@ return static function (RoutingConfigurator $routes): void {
         ->controller([LogoutController::class, 'logout']);
     $routes->add(RoutesEnum::LoggedOut->name, RoutesEnum::LoggedOut->value)
         ->controller([LoggedOutController::class, 'main']);
+    $routes->add(RoutesEnum::LoggedIn->name, RoutesEnum::LoggedIn->value)
+        ->controller([LoggedInController::class, 'main']);
 
     // Legacy Routes
     $routes->add(LegacyRoutesEnum::LegacyValidate->name, LegacyRoutesEnum::LegacyValidate->value)
@@ -32,4 +35,6 @@ return static function (RoutingConfigurator $routes): void {
         ->controller([LogoutController::class, 'logout']);
     $routes->add(LegacyRoutesEnum::LegacyLoggedOut->name, LegacyRoutesEnum::LegacyLoggedOut->value)
         ->controller([LoggedOutController::class, 'main']);
+    $routes->add(LegacyRoutesEnum::LegacyLoggedIn->name, LegacyRoutesEnum::LegacyLoggedIn->value)
+        ->controller([LoggedInController::class, 'main']);
 };
