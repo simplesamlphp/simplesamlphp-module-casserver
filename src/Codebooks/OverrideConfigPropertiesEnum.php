1a7e2dcfe6db08fc1e8c57124022839f5abf20f2
Ioannis Igoumenos <ioigoume@gmail.com>
2025-01-09 15:47:57 +0200

    Add missing enum file

---
+++ src/Codebooks/OverrideConfigPropertiesEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\casserver\Codebooks;
+
+enum OverrideConfigPropertiesEnum: string
+{
+    case Attrname = 'attrname';
+    case AttributesToTransfer = 'attributes_to_transfer';
+    case Authproc = 'authproc';
+    case ServiceTicketExpireTime = 'service_ticket_expire_time';
+}
